16e56db42951d63f8884af4c77194d5e117f7c83
GuaGua0318 <15972660547@163.com>
2024-05-07 15:45:26 +0800

    feat:背景切换组件

---
+++ src/components/Bg/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg">
+    <img class="img" :src="`../src/assets/v${props.bgCount}.jpg`" alt="">
+    
+  </div>
+  {{ props.bgCount }}
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue';
+
+const props = defineProps(['bgCount'])
+
+</script>
+
+<style lang="scss" scoped>
+.bg {
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: -999999;
+
+  .img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    home
-  </div>
+<Bg :bgCount="bgCount"></Bg>
+<button @click="toggleBg">切换壁纸</button>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import Bg from "../../components/Bg/index.vue";
+import { ref } from "vue";
+
+const bgCount = ref(1)
+ 
+const toggleBg = () => {
+  if(bgCount.value === 2){
+    bgCount.value = 1;
+  }else {
+    bgCount.value++;
+  }
+}
 
 </script>
 
