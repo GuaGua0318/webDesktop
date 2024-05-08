dd960967e30717c6c9d0930d6197b860913f261b
GuaGua0318 <15972660547@163.com>
2024-05-08 17:46:27 +0800

    图片引入方式修改

--- src/components/Bg/index.vue
+++ src/components/Bg/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="bg">
     <div v-if="props.bgCount === 0" class="img" style="background-color: #333;"></div>
-    <img v-else class="img" :src="`@/assets/v${props.bgCount}.jpg`" alt="">
+    <img v-else class="img" :src="imgArr[props.bgCount - 1]"  alt="">
   </div>
 </template>
 
 <script setup>
 import { defineProps, ref } from 'vue';
+import img1 from '@/assets/v1.jpg'
+import img2 from '@/assets/v2.jpg'
 
 const props = defineProps(['bgCount'])
+const imgArr = [img1,img2]
 
 </script>
 
