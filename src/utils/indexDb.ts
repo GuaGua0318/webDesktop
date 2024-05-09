961a9dac3aa3b4c529f40916538d0288c394ba62
GuaGua0318 <15972660547@163.com>
2024-05-09 21:33:50 +0800

    feat:小记添加查询与编辑操作

--- src/utils/indexDb.ts
+++ src/utils/indexDb.ts
@@ -5,7 +5,7 @@
  * @param {string} version 数据库的版本
  * @return {object} 该函数会返回一个数据库实例
  */
-export function openDB(dbName:any, version = 1) {
+export function openDB(dbName: any, version = 1) {
   return new Promise((resolve, reject) => {
     //  兼容浏览器
     var indexedDB =
@@ -33,20 +33,15 @@ export function openDB(dbName:any, version = 1) {
       db = event.target.result; // 数据库对象
       var objectStore;
       // 创建存储库
-      objectStore = db.createObjectStore("signalChat", {
-        keyPath: "sequenceId", // 这是主键
-        // autoIncrement: true // 实现自增
+      objectStore = db.createObjectStore("note", {
+        keyPath: "noteId", // 这是主键
+        autoIncrement: true, // 实现自增
       });
       // 创建索引，在后面查询数据的时候可以根据索引查
-      objectStore.createIndex("link", "link", { unique: false }); 
-      objectStore.createIndex("sequenceId", "sequenceId", { unique: false });
-      objectStore.createIndex("messageType", "messageType", {
-        unique: false,
-      });
+      objectStore.createIndex("content", "content", { unique: false });
     };
   });
 }
-
 
 /**
  * 新增数据
@@ -54,7 +49,7 @@ export function openDB(dbName:any, version = 1) {
  * @param {string} storeName 仓库名称
  * @param {string} data 数据
  */
-export function addData(db:any, storeName, data) {
+export function addData(db: any, storeName, data) {
   var request = db
     .transaction([storeName], "readwrite") // 事务对象 指定表格名称和操作模式（"只读"或"读写"）
     .objectStore(storeName) // 仓库对象
@@ -69,3 +64,76 @@ export function addData(db:any, storeName, data) {
   };
 }
 
+/**
+ * 通过游标读取数据
+ * @param {object} db 数据库实例
+ * @param {string} storeName 仓库名称
+ */
+export function cursorGetData(db, storeName) {
+  let list = [];
+  var store = db
+    .transaction(storeName, "readwrite") // 事务
+    .objectStore(storeName); // 仓库对象
+  var request = store.openCursor(); // 指针对象
+  // 游标开启成功，逐行读数据
+  return new Promise((resolve, reject) => {
+    request.onsuccess = function (e) {
+      var cursor = e.target.result;
+      console.log(cursor);
+      if (cursor) {
+        // 必须要检查
+        list.push(cursor.value);
+        cursor.continue(); // 遍历了存储对象中的所有内容
+      } else {
+        console.log("游标读取的数据：", list);
+        resolve(list); 
+      }
+    };
+  });
+}
+
+
+
+/**
+ * 通过主键读取数据
+ * @param {object} db 数据库实例
+ * @param {string} storeName 仓库名称
+ * @param {string} key 主键值
+ */
+export function getDataByKey(db, storeName, key) {
+  return new Promise((resolve, reject) => {
+    var transaction = db.transaction([storeName]); // 事务
+    var objectStore = transaction.objectStore(storeName); // 仓库对象
+    var request = objectStore.get(key); // 通过主键获取数据
+
+    request.onerror = function (event) {
+      console.log("事务失败");
+    };
+
+    request.onsuccess = function (event) {
+      console.log("主键查询结果: ", request.result);
+      resolve(request.result);
+    };
+  });
+}
+
+/**
+ * 通过主键删除数据
+ * @param {object} db 数据库实例
+ * @param {string} storeName 仓库名称
+ * @param {object} id 主键值
+ */
+function deleteDB(db, storeName, id) {
+  var request = db
+    .transaction([storeName], "readwrite")
+    .objectStore(storeName)
+    .delete(id);
+
+  request.onsuccess = function () {
+    console.log("数据删除成功");
+  };
+
+  request.onerror = function () {
+    console.log("数据删除失败");
+  };
+}
--- src/views/Note/index.vue
+++ src/views/Note/index.vue
@@ -4,16 +4,16 @@
       <div class="grid-content ep-bg-purple" />
       <el-card class="card" style="background-color: rgba(255, 255, 255, 0.5);">
         <template #header>
-          <div class="card-header">
+          <div class="card-header" style="color: #333;">
             <span>小记</span>
-            <el-button type="primary" class="addBtn"><el-icon>
+            <el-button @click="() => addEdit()" type="primary" class="addBtn"><el-icon>
                 <Edit />
               </el-icon>添加</el-button>
           </div>
         </template>
-        <div class="cardBody">
-          <div class="item">
-
+        <div class="cardBody" style="color: #333;">
+          <div class="item" v-for="item in noteList" v-html="item.content" style="cursor: pointer;"
+            @click="() => getNoteDetail(item.noteId)" @contextmenu.prevent="() => rightMenu()">
           </div>
         </div>
 
@@ -31,26 +31,63 @@
 import { Edit } from '@element-plus/icons-vue';
 import { config } from '../../utils/editConfig'
 import FroalaEditor from 'froala-editor';
-import { ref } from 'vue';
-import {openDB,addData} from '../../utils/indexDb';
+import { ref, reactive, onMounted } from 'vue';
+import { openDB, addData, cursorGetData, getDataByKey } from '../../utils/indexDb';
+
+interface editContent {
+  content: string,
+  noteId: number
+}
 
 const editContent = ref<any>(null)
+const noteList = ref<any>(null)
+
+onMounted(async () => {
+  const desktop = await openDB('desktop');
+  //获取note列表
+  const noteArr = await cursorGetData(desktop, 'note')
+  noteList.value = noteArr
+})
+
+//查询单项note详情
+const getNoteDetail = async (id: number) => {
+  const desktop = await openDB('desktop');
+  const test = <editContent>await getDataByKey(desktop, 'note', id)
+  editContent.value = test.content;
+}
+
+//点击添加时候在内存中添加一条note，不保存退出即清除
+const addEdit = () => {
+  editContent.value = '';
+  noteList.value.unshift({ content: '无内容或未保存' })
+}
+
+//鼠标右击菜单
+const rightMenu = (event) => {
+  event.preventDefault()
+  console.log('testtest-----')
+}
+
+//删除一条note
+// const deleteNote = () => {
+
+// }
 
 
-
-FroalaEditor.DefineIcon('save', {NAME: 'info', SVG_KEY: "help"});
-        FroalaEditor.RegisterCommand('save', {
-          title: '保存',
-          focus: false,
-          undo: false,
-          refreshAfterCallback: false,
-          //保存小记
-          callback: async function () {
-            const test = await openDB('test')
-            console.log('test-----',test)
-            await addData(test,'signalChat',{link:'1',sequenceId:'2',messageType:'3'})
-          }
-        });
+FroalaEditor.DefineIcon('save', { NAME: 'info', SVG_KEY: "help" });
+FroalaEditor.RegisterCommand('save', {
+  title: '保存',
+  focus: false,
+  undo: false,
+  refreshAfterCallback: false,
+  //保存小记
+  callback: async function () {
+    const desktop = await openDB('desktop')
+    await addData(desktop, 'note', { content: editContent.value })
+    const noteArr = await cursorGetData(desktop, 'note')
+    noteList.value = noteArr
+  }
+});
 
 </script>
 
